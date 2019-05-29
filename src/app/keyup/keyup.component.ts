a9a472cb67a6e24d0907aea959212f3096babc3a
mougithub19 <moubusiness18@gmail.com>
2019-05-29 15:37:15 +0530

    2 files modified

--- src/app/keyup/keyup.component.ts
+++ src/app/keyup/keyup.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-keyup',
   templateUrl: './keyup.component.html',
-  styleUrls: ['./keyup.component.css']
+  styleUrls: ['./keyup.component.css'],
 })
 export class KeyupComponent implements OnInit {
 
